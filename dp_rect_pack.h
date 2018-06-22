a4a7131407c95f9426eedddce40ac71f042677e6
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2018-06-22 19:25:10 +0300

    Update copyright year

--- dp_rect_pack.h
+++ dp_rect_pack.h
@@ -2,7 +2,7 @@
  * Rectangle packing library.
  * v1.0.0
  *
- * Copyright (c) 2017 Daniel Plakhotich
+ * Copyright (c) 2017-2018 Daniel Plakhotich
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
